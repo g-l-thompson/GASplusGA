dd39f302988fb69c2b69cfc786a3d36ecb89f830
Gary Thompson <glt@ucla.edu>
2019-07-17 18:46:47 -0700

    First attempt at query, with hardcoded params.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-function hello2() {
-  Logger.log('Hello world from subdirectory.');
+function tester() {
+  Logger.log('Hello world.');
+  makeNewSheet("parameters");
+}
+
+// details on GA APIs for Apps Script found here: 
+//   https://developers.google.com/apps-script/advanced/analytics
+//
+// test account: 45095774
+// test profile: 198056905
+// table id: "ga:198056905"
+// tracking id: UA-45095774-27
+
+function getReportDataForProfile() {
+
+  var profileId = 198056905;
+  var tableId = 'ga:' + profileId;
+  var startDate = '2019-06-01';
+  var endDate = '2019-06-30';
+
+  var optArgs = {
+    'dimensions': 'ga:browser',              // Comma separated list of dimensions.
+    'metrics': 'ga:users,ga:sessions',
+    'sort': '-ga:users,-ga:sessions',       // Sort by users descending, then sessions descending
+    'start-index': '1',
+    'max-results': '250'                     // Display the first 250 results.
+  };
+
+  // Make a request to the API.
+  var results = Analytics.Data.Ga.get(
+      tableId,                    // Table id (format ga:xxxxxx).
+      startDate,                  // Start-date (format yyyy-MM-dd).
+      endDate,                    // End-date (format yyyy-MM-dd).
+      'ga:sessions,ga:pageviews', // Comma seperated list of metrics.
+      optArgs);
+
+  if (results.getRows()) {
+    var anything = 1;
+    anything++;
+    outputToSpreadsheet(results);
+    return results;
+
+  } else {
+    throw new Error('No views (profiles) found');
+  }
 }
--- src/sheetTools.js
+++ src/sheetTools.js
@@ -1,4 +1,18 @@
-// functions to add data to a spreasheet will ge here
-function hello() {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("hello");
+// Fnuctions to work with Google drive spreadsheets.
+
+function makeNewSheet(sheetName) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(sheetName);
+    results = Analytics.Data.Ga.get
 }
+
+function outputToSpreadsheet(results) {
+  
+    // Print the headers.
+    sheet = SpreadsheetApp.getActiveSpreadsheet();
+    sheet.getRange(1, 1, 1, headerNames.length)
+        .setValues([headerNames]);
+  
+    // Print the rows of data.
+    sheet.getRange(2, 1, results.getRows().length, headerNames.length)
+        .setValues(results.getRows());
+  }
